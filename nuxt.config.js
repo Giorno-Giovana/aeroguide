3601dbcf000a2a946c0434f89c693953e3100d63
Alex Zaver <zaverclub@gmail.com>
2021-04-25 04:13:53 +0300

    Добавлен глобальный css

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aeroguide',
+    title: 'Aeroguide',
     htmlAttrs: {
       lang: 'en',
     },
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['ant-design-vue/dist/antd.css', '~/static/default.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
