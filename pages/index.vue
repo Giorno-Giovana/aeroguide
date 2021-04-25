a7ab0a27454f086c25f22792831418c0b051d13d
Alex Zaver <zaverclub@gmail.com>
2021-04-25 04:19:15 +0300

    Добавлен редирект с корневого роута

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,10 @@
-<template>
-  <div class="flex flex-col mt-20">
-    <div class="flex justify-center">
-      <h1 class="text-3xl">Ваш Аэрогид</h1>
-    </div>
-    <ul class="m-auto mt-12 list-disc">
-      <li>Мы не дадим вавм пропустить рейс</li>
-      <li>Покажем ближайшие магазины</li>
-      <li>Укажем на ваш выход к самолёту</li>
-      <li>Скрасим досуг перед вылетом</li>
-      <li>И многое, многое другое</li>
-    </ul>
-    <div class="w-60 m-auto mt-32 flex flex-col">
-      <input
-        v-model="trackNumber"
-        type="text"
-        placeholder="Введитие номер рейса"
-        class="h-14 border placeholder-black text-center rounded-xl"
-      />
-      <button class="h-14 mt-3 border rounded-xl bg-gray-300">Войти</button>
-    </div>
-  </div>
-</template>
+<template><div></div></template>
 
 <script>
 export default {
-  data() {
-    return {
-      trackNumber: '',
-    }
+  name: 'Index',
+  created() {
+    this.$router.push('/login')
   },
 }
 </script>
-
-<style scoped></style>
