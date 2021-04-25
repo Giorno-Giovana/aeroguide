7010b8c476fc2401dcef49d770f346f277f16cb1
Alex Zaver <zaverclub@gmail.com>
2021-04-25 04:18:21 +0300

    Добавлена страница выбора этаже

---
+++ pages/floors.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-center text-xl font-semibold mt-20">На каком вы этаже?</h1>
+    <div class="floor" @click="handleFloorClick(1)">1</div>
+    <div class="floor" @click="handleFloorClick(2)">2</div>
+    <div class="floor" @click="handleFloorClick(3)">3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Floors',
+  methods: {
+    handleFloorClick(floor) {
+      this.$store.commit('user/setFloor', floor)
+      this.$router.push('/feed')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.floor {
+  @apply m-auto mt-10 rounded-full text-5xl text-black h-24 w-24 flex items-center justify-center border border-4 border-black;
+}
+</style>
